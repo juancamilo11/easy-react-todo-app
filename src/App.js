e334ce7cce7c12fc72044c792d19a539ae48ff77
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-11-04 03:17:35 -0500

    Se agregó bootstrap y título al proyecto

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Main App</h1>
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-8">
+          <h1>ToDo List</h1>
+        </div>
+        <div className="col-4">
+          <h2>New ToDo List</h2>
+        </div>
+      </div>
     </div>
   );
 }
